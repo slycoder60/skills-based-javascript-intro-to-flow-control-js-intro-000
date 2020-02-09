95d407fbc00fc1fb250c0b7500db2e80fe56b0b2
Omar Farah <farah.omar8787@gmail.com>
2020-02-09 22:57:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,13 +27,13 @@ function ageChecker(age) {
 
 }
 
-/*function ternaryTeenager(age) {
+function ternaryTeenager(age) {
   if(age > 13 && age <19){
   return 'You are not a teenager'}
     return 'Youare not a teenager'
 
 
-}*/
+}
 
 function switchAge(age) {
 
