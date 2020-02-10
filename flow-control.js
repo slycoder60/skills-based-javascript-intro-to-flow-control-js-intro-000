d4efdd71b149b76670ed2ee073f419b1057ca390
Omar Farah <farah.omar8787@gmail.com>
2020-02-10 00:27:51 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -38,7 +38,7 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch (age){
     case age<13 && ag>19:
-    
+
     return'You are a teenager'
     default:
     return 'You have an age'
